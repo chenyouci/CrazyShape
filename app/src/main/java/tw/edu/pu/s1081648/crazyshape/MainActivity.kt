e20f593540787896cc40b5d8e0c3d66b6c19741b
chenyouci <chenyouci@protonmail.com>
2021-05-21 14:13:53 +0800

    2021上機考v1_陳佑齊

--- app/src/main/java/tw/edu/pu/s1081648/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1081648/crazyshape/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -23,6 +24,7 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
         })
+
 
     }
 }
