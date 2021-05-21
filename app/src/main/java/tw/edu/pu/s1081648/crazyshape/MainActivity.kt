244e324bc094cc24a4880e30b6ce090144adf37a
chenyouci <chenyouci@protonmail.com>
2021-05-21 12:05:21 +0800

    2021上機考v0_陳佑齊

---
+++ app/src/main/java/tw/edu/pu/s1081648/crazyshape/MainActivity.kt
@@ -0,0 +1,28 @@
+package tw.edu.pu.s1081648.crazyshape
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(applicationContext, "作者：陳佑齊", Toast.LENGTH_LONG).show()
+
+        imgNext.setOnLongClickListener(object : View.OnLongClickListener {
+            override fun onLongClick(p0: View?): Boolean {
+                intent = Intent(this@MainActivity, GameActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        })
+
+    }
+}
